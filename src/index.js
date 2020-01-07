876491b760c6e0dffad3443d94e6f730076352c0
Miss-Cheese <nat.galligan@gmail.com>
2020-01-07 17:28:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+const submitButton = document.querySelectorAll("input")[2]
+const textField = document.getElementById("new-task-description")
+const task = document.getElementById("tasks")
+const dueDateField = document.getElementById("due-date")
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  submitButton.addEventListener("click", function(event) {
+
+    event.preventDefault()
+
+    function createLi() { 
+    const li = document.createElement("li")
+    li.innerHTML = `${textField.value}<br>
+    ${dueDateField.value}<br>
+    <button class="delete-button" type="delete" value='delete'>x</button>
+    `
+    return li
+    }
+
+    const selectedLi = createLi()
+    task.append(selectedLi)
+    const deleteButton = selectedLi.getElementsByClassName("delete-button")[0]
+    deleteButton.addEventListener("click", function(event) {
+       selectedLi.remove()
+    })
+  })
+  
+
 });
+
+
